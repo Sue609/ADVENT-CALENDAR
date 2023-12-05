1c3619aa9f1d8f49f527b33851379bd5b49e2efe
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-12-05 16:06:37 +0300

    trying to upgrade the landing page and not make it too basic

--- Advent/snowflake.js
+++ Advent/snowflake.js
@@ -7,13 +7,25 @@ function createSnowflakesAndStarsInText() {
         const snowflake = document.createElement('div');
         snowflake.classList.add('snowflake');
         snowflake.style.animationDelay = `${Math.random() * 5}s`; // Randomize animation delay
+        setRandomPosition(snowflake); // Set random position within intro-text
         introText.appendChild(snowflake);
 
         const redStar = document.createElement('div');
         redStar.classList.add('red-star');
         redStar.style.animationDelay = `${Math.random() * 5}s`; // Randomize animation delay
+        setRandomPosition(redStar); // Set random position within intro-text
         introText.appendChild(redStar);
     }
+}
+
+// Function to set random position within the intro-text
+function setRandomPosition(element) {
+    const introTextWidth = document.querySelector('.intro-text').offsetWidth;
+    const introTextHeight = document.querySelector('.intro-text').offsetHeight;
+    const randomX = Math.floor(Math.random() * introTextWidth);
+    const randomY = Math.floor(Math.random() * introTextHeight);
+    element.style.left = `${randomX}px`;
+    element.style.top = `${randomY}px`;
 }
 
 // Call the function to generate snowflakes and red stars within text area
