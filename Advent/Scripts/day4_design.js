9fab079db006031f20fbc4b0177336fc4787b6c1
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-12-08 17:03:02 +0300

    redisigning the pen to stop drawing when the user hoovers over the canvas

--- Advent/Scripts/day4_design.js
+++ Advent/Scripts/day4_design.js
@@ -8,10 +8,6 @@ function DesignArt() {
     canvas.width = 700;
     canvas.height = 500;
 
-    let isResizing = false;
-    let originalWidth, originalHeight;
-    let originalX, originalY;
-
     let isDrawing = false;
     let penColor = '#000000';
     let penSize = 5;
@@ -20,48 +16,16 @@ function DesignArt() {
 
     // Event listener for mouse down
     canvas.addEventListener('mousedown', function(e) {
-        if (e.offsetX > canvas.width - 10 && e.offsetY > canvas.height - 10) {
-            isResizing = true;
-            originalWidth = canvas.width;
-            originalHeight = canvas.height;
-            originalX = e.clientX;
-            originalY = e.clientY;
-        } else {
-            isDrawing = true;
+        isDrawing = document.getElementById('penTool').classList.contains('active');
+        if (isDrawing) {
             ctx.beginPath();
             ctx.moveTo(e.offsetX, e.offsetY);
-        }
+        }   
     });
 
     // Event listener for mouse move
     window.addEventListener('mousemove', function(e) {
-        if (isResizing) {
-            const newWidth = originalWidth + (e.clientX - originalX);
-            const newHeight = originalHeight + (e.clientY - originalY);
-
-            if (newWidth > 50 && newHeight > 50) {
-                canvas.width = newWidth;
-                canvas.height = newHeight;
-
-                const tempCanvas = document.createElement('canvas');
-                const tempCtx = tempCanvas.getContext('2d');
-                tempCanvas.width = newWidth;
-                tempCanvas.height = newHeight;
-                tempCtx.drawImage(canvas, 0, 0);
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.drawImage(tempCanvas, 0, 0);
-
-                drawingData.forEach(function(item) {
-                    if (item.type === 'pen') {
-                        redrawPen(item.data);
-                    } else if (item.type === 'text') {
-                        redrawText(item.data);
-                    } else if (item.type === 'rectangle') {
-                        redrawRectangle(item.data);
-                    }
-                });
-            }
-        } else if (isDrawing) {
+        if (isDrawing) {
             ctx.lineTo(e.offsetX, e.offsetY);
             ctx.strokeStyle = penColor;
             ctx.lineWidth = penSize;
@@ -74,34 +38,16 @@ function DesignArt() {
 
     // Event listener for mouse up
     window.addEventListener('mouseup', function() {
-        isResizing = false;
         isDrawing = false;
     });
 
-    // Drawing tools functionality...
-
-    function redrawPen(data) {
-        ctx.strokeStyle = data.color;
-        ctx.lineWidth = data.size;
-        ctx.lineCap = 'round';
-        ctx.lineJoin = 'round';
-        ctx.lineTo(data.x, data.y);
-        ctx.stroke();
-    }
-
-    // Other drawing tool functions (text, rectangle)...
-
-    function redrawText(data) {
-        ctx.font = '20px Arial';
-        ctx.fillStyle = data.color;
-        ctx.fillText(data.text, data.x, data.y);
-    }
-
-    function redrawRectangle(data) {
-        ctx.fillStyle = data.color;
-        ctx.fillRect(data.x, data.y, data.width, data.height);
-    }
-
+    // Pen tool button functionality
+    const penToolButton = document.getElementById('penTool');
+    penToolButton.addEventListener('click', function() {
+        // Toggle 'active' class to indicate pen tool selection
+        penToolButton.classList.toggle('active');
+        // Other logic to set penColor, penSize, etc.
+    });
     
     function showChristmasGIFs() {
         const showGIFsBTN = document.getElementById('showChristmasGIFs');
