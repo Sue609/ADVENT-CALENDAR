7d3f0cb815430c4bb3e3a03da5e648a4b3c977ce
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-12-10 16:48:02 +0300

    perfecting a letter to future self on day 20

--- Advent/Scripts/day20.js
+++ Advent/Scripts/day20.js
@@ -9,38 +9,46 @@ function futureIntrospection() {
         const careerVision = document.getElementById('careerVision').value;
         const personalGoals = document.getElementById('personalGoals').value;
         const personalMessage = document.getElementById('personalMessage').value;
-        const currentLocation = document.getElementById('currentLocation').value; // Added field for current location
-        const currentEmotion = document.getElementById('currentEmotion').value; // Added field for current emotion
-        const currentAchievement = document.getElementById('currentAchievement').value; // Added field for current achievement
+        const currentLocation = document.getElementById('currentLocation').value;
+        const currentEmotion = document.getElementById('currentEmotion').value;
+        const currentAchievement = document.getElementById('currentAchievement').value;
 
         // Expanded personalized story using the user inputs
-        const storyText = `Dear ${name},
-            I hope this letter finds you well! 😊 
+        const storyText = 
+            `Dear ${name},
+            I hope this letter finds you well!
             As I pen down these words, my heart swells with overwhelming emotions and gratitude that words
             can hardly express. Reflecting on 2023, I'm filled with immense happiness and a sense of
             accomplishment as I ponder our journey.
-            Those sleepless nights, battling through challenges like ${challenges}, became our strength,
-            molding us with invaluable lessons that have sculpted us into who we are today.
+
+            Those sleepless nights, battling through challenges like ${challenges}, became
+            our strength, molding us with invaluable lessons that have sculpted us into who
+            we are today.
+
             Remember when we took that leap of faith with ALX-Africa? Who knew it would be a blessing in
             disguise? The challenges, uncertainties, and moments of doubt led to where we stand today.
             Our vision of ${careerVision} is clearer than ever. 
+            
             Our journey has been a rollercoaster, shaping us for the better. Once-distant personal goals
             are now within reach. I'm excited about the future. In 10 years, I envision us achieving
             ${personalGoals}.
             The sheer determination we've shown gives me confidence.
+            
             Each experience, including ${lesson}, contributed to sculpting us into who we are today.
-            The message that resonates within us, "${personalMessage}," has been our guiding compass through
-            storms and calm seas alike. We navigated through obstacles, aligning ourselves perfectly
-            with our vision of ${careerVision}.
+            The message that resonates within us, "${personalMessage}," has been our guiding compass
+            through storms and calm seas alike. We navigated through obstacles, aligning ourselves
+            perfectly with our vision of ${careerVision}.
+
             Envisioning ourselves 10 years from now, standing in ${currentLocation}, overwhelmed
             with ${currentEmotion}, we celebrate the triumphs, including ${currentAchievement}.
             Remember the resilience we've shown, the message: ${personalMessage}.
             It's been our beacon through tough times.
+
             Let's revel in our achievements, cherish the growth, and embrace the happiness that comes
             from our resilience and hard work.
-            Here's to the future, to continued success, and the convergence of dreams and reality. 🚀✨
+            Here's to the future, to continued success, and the convergence of dreams and reality.
             Our decision to take that leap of faith was nothing short of destiny's nudge,
-            and I'm forever grateful for it. 🙏With unending pride, gratitude, and anticipation
+            and I'm forever grateful for it. With unending pride, gratitude, and anticipation
             for the journey ahead.        
             Warm regards,
             ${name}`;
@@ -51,49 +59,61 @@ function futureIntrospection() {
 
     // Get the download button element
     const downloadButton = document.getElementById('downloadButton');
-
     function downloadAsPDF() {
         const doc = new jsPDF({
-            orientation: 'portrait', // Set the orientation to portrait
-            unit: 'pt', // Use points as the unit
-            lineHeight: 1.2 // Adjust line height for better spacing
+            orientation: 'portrait',
+            unit: 'pt',
+            lineHeight: 1.2
         });
-
-        // Retrieve text content from output div
+    
         const storyText = document.getElementById('storyOutput').textContent;
-
-        // Split text into an array of paragraphs based on line breaks ('\n')
-        let paragraphs = storyText.split('\n');
-
-        // Set initial y position for text
-        let yPos = 50; // Start lower to give space for title and margins
-
-        // Set initial font size and line height
+    
+        let yPos = 50;
         let fontSize = 12;
-        let lineHeight = 1.2;
-
-        // Add each paragraph to the document
-        paragraphs.forEach(paragraph => {
-            let textLines = doc.setFontSize(fontSize).splitTextToSize(paragraph, doc.internal.pageSize.width - 40); // Adjust the width and margins
-
-            // Check if the text fits within the page, otherwise reduce the font size
-            while (doc.getTextDimensions(textLines[0]).h * textLines.length > doc.internal.pageSize.height - 40) {
-                fontSize -= 0.5;
-                textLines = doc.setFontSize(fontSize).splitTextToSize(paragraph, doc.internal.pageSize.width - 40);
-            }
-
-            // Add each line of the paragraph with adjusted font size
+        let lineHeight = 1.3;
+    
+        const splitText = (text, maxWidth) => {
+            return doc.setFontSize(fontSize).splitTextToSize(text, maxWidth);
+        };
+    
+        const addTextToDoc = (textLines) => {
             textLines.forEach(line => {
-                doc.text(20, yPos, line); // Adjust the margins as needed
-                yPos += fontSize * lineHeight; // Increase yPos based on font size and line height
+                doc.text(20, yPos, line);
+                yPos += fontSize * lineHeight;
             });
-
-            yPos += 10; // Add extra space between paragraphs
-        });
-
-        // Save the PDF
-        doc.save('FutureLetter.pdf');
+        };
+    
+        const totalPages = () => {
+            let totalHeight = 0;
+            let remainingText = storyText;
+    
+            while (remainingText.trim().length > 0) {
+                const textLines = splitText(remainingText, doc.internal.pageSize.width - 40);
+                const textHeight = doc.getTextDimensions(textLines[0]).h * textLines.length;
+    
+                if (yPos + textHeight > doc.internal.pageSize.height - 40) {
+                    doc.addPage();
+                    yPos = 50;
+                } else {
+                    totalHeight += textHeight;
+                    addTextToDoc(textLines);
+                    yPos += 10;
+                    remainingText = remainingText.substring(textLines.join('').length);
+                }
+            }
+            return Math.ceil(totalHeight / (doc.internal.pageSize.height - 40));
+        };
+    
+        const totalPagesCount = totalPages();
+    
+        if (totalPagesCount > 1) {
+            doc.deletePage(totalPagesCount);
+            doc.save('FutureLetter.pdf');
+        } else {
+            doc.save('FutureLetter.pdf');
+        }
     }
+    
 
     // Attach click event to the download button to trigger PDF download
     downloadButton.addEventListener('click', downloadAsPDF);
