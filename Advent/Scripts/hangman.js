0306157fa1c0c1a5cfae2640b8038ff0f3e0fe3d
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-12-11 11:25:47 +0300

    listing the hangman challenge hints and their proper names

--- Advent/Scripts/hangman.js
+++ Advent/Scripts/hangman.js
@@ -107,87 +107,41 @@ function hangmanGame () {
 
 const wordList = [
     {
-        word: "guitar",
-        hint: "A musical instrument with strings."
+        word: "swaniker",
+        hint: "The founder of ALX Africa. But do you know his second name "
     },
     {
-        word: "oxygen",
-        hint: "A colorless, odorless gas essential for life."
+        word: "dohardthings",
+        hint: "ALX's rallying call urging individuals to embrace challenges, break barriers, and grow through perseverance and determination."
     },
+
     {
-        word: "mountain",
-        hint: "A large natural elevation of the Earth's surface."
+        word: "christmas",
+        hint: "During this festive season, it's a time of giving, joy, and merriment celebrated worldwide with decorations, gifts, and gatherings."
     },
+
     {
-        word: "painting",
-        hint: "An art form using colors on a surface to create images or expression."
+        word: "mastercard",
+        hint: "Financial empowerment partner supporting ALX's mission to cultivate leadership and innovation through access to resources and opportunities.."
     },
+
     {
-        word: "astronomy",
-        hint: "The scientific study of celestial objects and phenomena."
+        word: "alx_ai",
+        hint: "What is the popular hashtag for the latest unique and visionary initiative meticulously designed by Kalkidan Betre and Julien Barbier that is available for ALX graduates"
     },
-    {
-        word: "football",
-        hint: "A popular sport played with a spherical ball."
-    },
-    {
-        word: "chocolate",
-        hint: "A sweet treat made from cocoa beans."
-    },
-    {
-        word: "butterfly",
-        hint: "An insect with colorful wings and a slender body."
-    },
-    {
-        word: "history",
-        hint: "The study of past events and human civilization."
-    },
-    {
-        word: "pizza",
-        hint: "A savory dish consisting of a round, flattened base with toppings."
-    },
-    {
-        word: "jazz",
-        hint: "A genre of music characterized by improvisation and syncopation."
-    },
-    {
-        word: "camera",
-        hint: "A device used to capture and record images or videos."
-    },
-    {
-        word: "diamond",
-        hint: "A precious gemstone known for its brilliance and hardness."
-    },
-    {
-        word: "adventure",
-        hint: "An exciting or daring experience."
-    },
-    {
-        word: "science",
-        hint: "The systematic study of the structure and behavior of the physical and natural world."
-    },
-    {
-        word: "bicycle",
-        hint: "A human-powered vehicle with two wheels."
-    },
-    {
-        word: "sunset",
-        hint: "The daily disappearance of the sun below the horizon."
-    },
-    {
-        word: "coffee",
-        hint: "A popular caffeinated beverage made from roasted coffee beans."
-    },
-    {
-        word: "dance",
-        hint: "A rhythmic movement of the body often performed to music."
-    },
-    {
-        word: "galaxy",
-        hint: "A vast system of stars, gas, and dust held together by gravity."
-    },
+    
     {
         word: "orchestra",
         hint: "A large ensemble of musicians playing various instruments."
     },
+
+    {
+        word: "Irene",
+        hint: "The Community whisperer Your #1 Cheerleader through your learning journey",
+    },
+
+    {
+        word: "mastercard",
+        hint: "Our partnership with __ Foundation allows us to offer eligible candidates access to world-class programme training at no cost"
+    }
 ];
